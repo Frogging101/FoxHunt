af71fa89a792f0ef7eca4118f566353d590dba17
John Brooks <john@fastquake.com>
2014-11-10 01:50:21 -0500

    game over, lives, score, images, sounds, a ton of stuff. 0.1 ready

--- game.py
+++ game.py
@@ -98,8 +98,10 @@ class Entity(object):
 class Turret(Entity):
     sprite = pygame.image.load("turret.png")
     def __init__(self):
+        #Timer, turret will fire when it reaches a certain random number
         self.shootTimer = 0
-        self.shootTimerEnd = random.randint(900,1500)
+        self.shootTimerEnd = random.randint(900,1325)
+        self.health = 100
         super(Turret,self).__init__(Turret.sprite)
     
     def canSeePlayer(self,app):
@@ -117,8 +119,7 @@ class Turret(Entity):
             colliderRect = pygame.Rect(ray.x+center[0],ray.y+center[1],1,1)
             #If the ray hits anything that isn't this turret
             if colliderRect.collidelist([tile for tile in 
-                                         app.level.collisionTiles 
-                                         if tile != self.rect]) != -1:
+                                         app.level.wallTiles]) != -1:
                 return False
             if colliderRect.colliderect(app.player.rect):
                 return True
@@ -132,8 +133,13 @@ class Turret(Entity):
                 self.shoot(app)
             self.shootTimer = 0
             self.shootTimerEnd = random.randint(900,1500)
+        if self.health <= 0:
+            app.level.turrets.remove(self)
+            app.score += 100
 
     def shoot(self,app):
+        #all this arithmetic is to make a vector from the center of
+        #the turret to the center of the player
         toPlayer = Vector(app.player.x+app.player.rect.width//2,
                           app.player.y+app.player.rect.height//2)-\
                           Vector(self.x+self.rect.width//2,self.y+self.rect.height//2)
@@ -144,13 +150,15 @@ class Turret(Entity):
 
 class Bullet(Entity):
     sprite = pygame.image.load("bullet.png")
-    def __init__(self,origin,direction):
+    def __init__(self,origin,direction,playerFired=False):
+        self.playerFired = playerFired
         left = Vector(-1,0)
+        #Angle to rotate bullet sprite
         angle = math.acos(Vector.dot(left,direction))*(180/math.pi)
         if direction.y < 0:
             angle = -angle
         self.sprite = pygame.transform.rotate(Bullet.sprite,angle)
-        self.velocity = 25*direction
+        self.velocity = 20*direction
         self.x = origin[0]
         self.y = origin[1]
         self.rect = self.sprite.get_rect()
@@ -158,11 +166,17 @@ class Bullet(Entity):
     def update(self,dt,app):
         if self.rect.collidelist(app.level.wallTiles) != -1:
             app.bullets.remove(self)
-        elif self.rect.colliderect(app.player.rect):
+        elif self.rect.colliderect(app.player.rect) and not self.playerFired:
             app.damageSound.play()
+            app.health -= random.randint(7,15)
             app.bullets.remove(self)
-        else:
-            super(Bullet,self).update(dt)
+        elif self.playerFired:
+            turretHit = self.rect.collidelist([turret.rect for turret in app.level.turrets])
+            if turretHit != -1: #if a turret was hit
+                app.bullets.remove(self)
+                app.level.turrets[turretHit].health -= 50
+                
+        super(Bullet,self).update(dt)
 
 class Room:
     def __init__(self,level):
@@ -189,7 +203,6 @@ class Map:
         self.numRooms = random.randint(6,12)
         self.rooms = []
         self.data = []
-        self.collisionTiles = []
         self.wallTiles = []
         self.turrets = []
 
@@ -266,13 +279,12 @@ class Map:
             if room == spawnRoom:
                 continue
             for tile in range(room.xSize*room.ySize):
-                if random.random() < 1/10:
+                if random.random() < 1/15:
                     tileX = room.x+tile%room.xSize
                     tileY = room.y+tile//room.xSize
                     self.data[tileX][tileY] = Map.TURRET
                     newTile = pygame.Rect(tileX*self.tilesize,tileY*self.tilesize,
                                           self.tilesize,self.tilesize)
-                    self.collisionTiles.append(newTile)
                     newTurret = Turret()
                     newTurret.move(tileX*Map.tilesize,tileY*Map.tilesize)
                     self.turrets.append(newTurret)
@@ -294,7 +306,6 @@ class Map:
                         if self.data[pos[0]][pos[1]] == Map.EMPTY:
                             self.data[pos[0]][pos[1]] = Map.WALL
                             newTile = pygame.Rect(pos[0]*self.tilesize,pos[1]*self.tilesize,self.tilesize,self.tilesize)
-                            self.collisionTiles.append(newTile)
                             self.wallTiles.append(newTile)
                 x += 1
             y += 1
@@ -319,51 +330,68 @@ class Application:
         self.width = 800
         self.height = 600
         self.screen = pygame.display.set_mode((self.width,self.height))
+        pygame.display.set_caption("FoxHunt v0.1")
         Bullet.sprite = Bullet.sprite.convert_alpha() #This is here because it has to be done after init
         self.bg_colour = 0,0,0
 
         #Entity representing the mouse, no model
         self.mouseEntity = Entity(None)
         self.mouseEntity.move(*pygame.mouse.get_pos())
-
-        self.score = 0
-        self.lives = 3
-        self.state = 0
-
-        self.cameraX = 0
-        self.cameraY = 0
-
-        self.level = Map()
+        #Tiles
         self.floorTile = pygame.image.load("floor.png")
         self.turretTile = pygame.image.load("turret.png")
         self.wallTile = pygame.image.load("wall.png")
-        self.player = Entity(pygame.image.load("player.png"))
-        self.player.move(self.level.spawnX,self.level.spawnY)
-        self.fox = Entity(pygame.image.load("fox.png"))
-        self.fox.move(self.level.foxX,self.level.foxY)
 
+        #Entities
+        self.player = Entity(pygame.image.load("player.png"))
+        self.fox = Entity(pygame.image.load("fox.png"))
+
+        #Sounds
         self.bulletSound = pygame.mixer.Sound("bullet.wav")
         self.bgm = pygame.mixer.music.load("bg.wav")
         self.damageSound = pygame.mixer.Sound("oof.wav")
-
-        self.bullets = []
-
+        self.pickupSound = pygame.mixer.Sound("pickup.wav")
+        self.winSound = pygame.mixer.Sound("eb-youwin.wav")
+        self.gameOverSound = pygame.mixer.Sound("eb-loss.wav")
+        self.dieSound = pygame.mixer.Sound("159408__noirenex__life-lost-game-over.wav")
+        
+        self.foximg = pygame.image.load("foxhunt_f.png")
         self.font = pygame.font.Font("PressStart2P.ttf",24)
-        pygame.mixer.music.play()
+        self.state = 2
 
+    def gameInit(self):
+        self.score = 0
+        self.lives = 3
+        self.state = 0
+        self.win = False
+        self.health = 100
+
+        self.cameraX = 0
+        self.cameraY = 0
+        
+        self.level = Map()
+        self.player.move(self.level.spawnX,self.level.spawnY)
+        self.fox.move(self.level.foxX,self.level.foxY)
+        self.bullets = []
+        
     def collideLevel(self,obj,direction):
         obj = pygame.Rect(obj)
         collideX = False
         collideY = False
 
         obj.x += 11*direction.x
-        if obj.collidelist(self.level.collisionTiles) != -1:
+        #If it collided with a wall or a turret while moving on the X axis
+        if obj.collidelist(self.level.wallTiles) != -1 or\
+           obj.collidelist([turret.rect for turret in self.level.turrets]) != -1:
             collideX = True
         obj.x -= 11*direction.x
         obj.y += 11*direction.y
-        if obj.collidelist(self.level.collisionTiles) != -1:
+        #do this again but in the Y direction
+        if obj.collidelist(self.level.wallTiles) != -1 or\
+           obj.collidelist([turret.rect for turret in self.level.turrets]) != -1:
             collideY = True
 
+        #kill movement if it would have hit a wall
         if collideX:
             direction.x = 0
         if collideY:
@@ -372,11 +400,11 @@ class Application:
     def signalBar(self):
         distance = (Vector(self.player.x,self.player.y)-\
                     Vector(self.fox.x,self.fox.y)).getMagnitude()
-        strength = min(10000.0/distance**2,1.0)
-        print strength,distance
+        strength = min(12000.0/distance**2,1.0)
         width = (400*strength)
         bar = pygame.Surface((width,30))
         bar.fill((0,255,0))
+        bar.set_alpha(0.75*255)
         return bar
 
     def run(self):
@@ -384,12 +412,22 @@ class Application:
         clock = pygame.time.Clock()
 
         while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-
             #Normal gameplay state
             if self.state == 0:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        running = False
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        origin = (self.player.x+self.player.rect.width//2,
+                                  self.player.y+self.player.rect.height//2)
+                        mousePos = pygame.mouse.get_pos()
+                        mouseX = mousePos[0]+self.cameraX
+                        mouseY = mousePos[1]+self.cameraY
+                        direction = (Vector(mouseX,mouseY)-\
+                                    Vector(origin[0],origin[1])).normalize()
+                        newBullet = Bullet(origin,direction,True)
+                        self.bulletSound.play()
+                        self.bullets.append(newBullet)
                 dt = clock.tick(30) #Limit to 30 FPS
 
                 keys = pygame.key.get_pressed()
@@ -412,12 +450,112 @@ class Application:
                     turret.update(dt,self)
                 for bullet in self.bullets:
                     bullet.update(dt,self)
+                
+                if self.player.rect.colliderect(self.fox.rect):
+                    self.pickupSound.play()
+                    self.win = True
+                    self.state = 1
+                    self.score += 1250
+                if self.lives < 0:
+                    self.state = 1
+                if self.health < 0:
+                    self.dieSound.play()
+                    pygame.time.delay(int(self.dieSound.get_length()*1000))
+                    self.player.move(self.level.spawnX,self.level.spawnY)
+                    self.lives -= 1      
+                    self.health = 100              
 
                 self.cameraX = self.player.x+self.player.rect.width//2-self.width//2
                 self.cameraY = self.player.y+self.player.rect.height//2-self.height//2
 
                 self.mouseEntity.move(*pygame.mouse.get_pos())
                 self.draw()
+
+            elif self.state == 1:
+                pygame.mixer.music.stop()
+                if self.win:
+                    sound = self.winSound
+                else:
+                    sound = self.gameOverSound
+                timer = 0
+                played = False
+                endgame = True
+                fadeSurface = pygame.Surface((self.width,self.height))
+                fadeSurface.fill((0,0,0))
+                fadeSurface.set_alpha(0)
+                while endgame:
+                    if timer > 1000 and not played:
+                        played = True
+                        sound.play()
+                    dt = clock.tick(30)
+                    timer += dt
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            running = False
+                            endgame = False
+                        if event.type == pygame.MOUSEBUTTONDOWN and timer > 2000:
+                            endgame = False
+                    fadeout = 5000
+                    fadeSurface.set_alpha(min(int((timer)*(255/fadeout)),255))
+                    self.screen.blit(fadeSurface,fadeSurface.get_rect())
+                    if timer > fadeout/3:
+                        if not self.win:
+                            GOtext = self.font.render("Game Over",True,(255,255,255))
+                        else:
+                            GOtext = self.font.render("Fox get! You win!",True,(255,255,255))
+                            foximgRect = self.foximg.get_rect()
+                            foximgRect = foximgRect.move(self.width//2-foximgRect.width//2,self.height//3+10)
+                            self.screen.blit(self.foximg,foximgRect)
+                        clickText = self.font.render("Click to return to start screen",True,(255,255,255))
+
+                        goTextRect = GOtext.get_rect()
+                        clickTextRect = clickText.get_rect()
+                        clickTextRect = clickTextRect.move(self.width/2.0-clickTextRect.width/2.0,
+                                                           self.height/4.0-clickTextRect.height/2.0+\
+                                                           goTextRect.height)
+                        goTextRect = goTextRect.move(self.width/2.0-goTextRect.width/2.0,
+                                                     self.height/4.0-goTextRect.height/2.0-\
+                                                     clickTextRect.height)
+                        scoreText = self.font.render("Score: "+str(self.score),True,(255,255,255))
+                        scoreTextRect = scoreText.get_rect()
+                        scoreTextRect = scoreTextRect.move(self.width-scoreTextRect.width,0)
+
+                        self.screen.blit(scoreText,scoreTextRect)
+                        self.screen.blit(GOtext,goTextRect)
+                        self.screen.blit(clickText,clickTextRect)
+                    pygame.display.flip()
+                sound.stop()
+                self.state = 2
+            
+            elif self.state == 2:
+                pygame.mixer.music.stop()
+                self.screen.fill((0,0,0))
+                menu = True
+                while menu:
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            running = False
+                            menu = False
+                        if event.type == pygame.MOUSEBUTTONDOWN:
+                            menu = False
+                    playText = self.font.render("Click to play",True,(255,255,255))
+                    playTextRect = playText.get_rect()
+                    playTextRect = playTextRect.move(self.width//2-playTextRect.width//2,
+                                                     self.height//3)
+                    
+                    sysfont = pygame.font.SysFont("monospace",24)
+                    attribText = sysfont.render("John Brooks | http://www.fastquake.com/",True,(255,255,255))
+                    attribTextRect = attribText.get_rect()
+                    attribTextRect = attribTextRect.move(self.width//2-attribTextRect.width//2,
+                                                         self.height-attribTextRect.height)
+                    
+                    self.screen.blit(attribText,attribTextRect)
+                    self.screen.blit(playText,playTextRect)
+                    
+                    pygame.display.flip()
+                pygame.mixer.music.rewind()
+                pygame.mixer.music.play(-1) #Play looping music
+                self.gameInit() #Initialize game world
 
     def draw(self):
         self.screen.fill(self.bg_colour)
@@ -427,11 +565,8 @@ class Application:
                 tilex = x*tilesize - self.cameraX
                 tiley = y*tilesize - self.cameraY
                 tilerect = pygame.Rect(tilex,tiley,tilesize,tilesize)
-                if tile == Map.FLOOR:
+                if tile == Map.FLOOR or tile == Map.TURRET:
                     self.screen.blit(self.floorTile,tilerect)
-                if tile == Map.TURRET:
-                    self.screen.blit(self.floorTile,tilerect)
-                    self.screen.blit(self.turretTile,tilerect)
                 if tile == Map.WALL:
                     self.screen.blit(self.wallTile,tilerect)
         playerScreenRect = self.player.rect.move(-self.cameraX,-self.cameraY)
@@ -439,11 +574,17 @@ class Application:
         for bullet in self.bullets:
             bulletScreenRect = bullet.rect.move(-self.cameraX,-self.cameraY)
             self.screen.blit(bullet.sprite,bulletScreenRect)
+        for turret in self.level.turrets:
+            turretScreenRect = turret.rect.move(-self.cameraX,-self.cameraY)
+            self.screen.blit(self.turretTile,turretScreenRect)
         foxScreenRect = self.fox.rect.move(-self.cameraX,-self.cameraY)
         self.screen.blit(self.fox.sprite,foxScreenRect)
 
         livesText = self.font.render("Lives: "+str(self.lives),True,(255,255,255))
         livesTextRect = livesText.get_rect()
+        healthText = self.font.render("Health: "+str(self.health),True,(255,255,255))
+        healthTextRect = healthText.get_rect()
+        healthTextRect = healthTextRect.move(0,livesTextRect.height)
 
         scoreText = self.font.render("Score: "+str(self.score),True,(255,255,255))
         scoreTextRect = scoreText.get_rect()
@@ -451,6 +592,7 @@ class Application:
 
         self.screen.blit(scoreText,scoreTextRect)
         self.screen.blit(livesText,livesTextRect)
+        self.screen.blit(healthText,healthTextRect)
 
         signalText = self.font.render("Signal",True,(255,255,255))
         signalTextRect = signalText.get_rect()
